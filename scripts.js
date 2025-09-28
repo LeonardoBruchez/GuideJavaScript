b42e8d7d275c6d8c84461a2c5bac4e7296c030af
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-09-28 19:48:41 -0300

    commit sumary

---
+++ scripts.js
@@ -0,0 +1,31 @@
+    // const marca = "Nike"
+    // const Número = 4
+    // console.log(marca)
+    
+    // let numero2 = 456;
+    // let stringConvertida2 = numero2.toString();
+
+    // const stringTester = "147"
+    // const ToNumber = Number(stringTester)
+    // console.log(typeof ToNumber)
+
+    // var n1 = 1536.4
+    // console.log(n1)
+    // console.log(n1.toFixed(2))
+
+
+    // var n2 = 1467.54
+    // console.log(n2)
+    // console.log(n2.toFixed(2).replace('.', ','))
+
+    // var n1 = 1543.5
+    // console.log(n1)
+    // console.log(n1.toLocaleString('pt-BR', {style: 'currency',currency:'BRL'}))
+    
+    // var n2 = 1543.5
+    // console.log(n2)
+    // console.log(n2.toLocaleString('pt-BR', {style: 'currency', currency: 'USD'}))
+
+    var n3 = 1543.5
+    console.log(n3)
+    console.log(n3.toLocaleString('pt-BR', {style: 'currency',currency:'EUR'}))
